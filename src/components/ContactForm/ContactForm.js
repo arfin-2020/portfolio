7d1d78b16279b2a16faae9ddd247447cde86dd70
Arfin Chowdhury Arif <arfinchowdhuryarif7@gmail.com>
2021-05-02 22:25:03 +0600

    dynamic mail set

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -25,31 +25,19 @@ const ContactForm = () => {
                         <h1 className="text-white">Get In Touch</h1>
                     </div>
                     <div className="col-md-9 mx-auto">
-                    <form className="contact-form" onSubmit={sendEmail}>
-                        
-                            <div className="form-group">
-                                <input type="text" name="user_email" className="form-control" placeholder="Email Address *"/>
-                            </div><br/>
-                            <div className="form-group">
-                                <input type="text" name="user_name"  className="form-control" placeholder="Subject *"/>
-                            </div><br/>
-                            <div className="form-group">
-                                <textarea name="message" className="form-control" id="" cols="30" rows="10" placeholder="Message *"></textarea>
-                            </div><br/>
-                            <div className="form-group text-center">
-                                <button type="button" className="fency-btn"> Submit </button>
-                            </div>
-                    </form>
-                        {/* <form className="contact-form" onSubmit={sendEmail}>
-                            <input type="hidden" name="contact_number" />
-                            <label>Name</label>
-                            <input type="text" name="user_name" />
-                            <label>Email</label>
-                            <input type="email" name="user_email" />
-                            <label>Message</label>
-                            <textarea name="message" />
-                            <input type="submit" value="Send" />
-                        </form> */}
+                         <form className="contact-form" onSubmit={sendEmail}>
+                             <input type="hidden" name="contact_number" />
+                                <div className = "form-group">
+                                <input type="text" name="user_name" className="form-control" placeholder="Name"/>
+                                </div><br/>
+                                <div className="form-group">
+                                <input type="email" name="user_email" className="form-control" placeholder="Email"/>
+                                </div><br/>
+                                <div className="form-group">
+                                    <textarea name="message" className="form-control" id="" cols="30" rows="10" placeholder="Message *"></textarea>
+                                </div><br/>
+                             <input type="submit" value="Send" />
+                        </form>
                     </div>
                 </div>
         </section>
